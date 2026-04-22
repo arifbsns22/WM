c090091e58037b2b893209273c05b2af690cc31b
Ariful Haque Mohsin <arifbsns22@gmail.com>
2026-04-22 12:31:05 +0600

    Video editing page UI

--- src/app/services/video-editing/page.tsx
+++ src/app/services/video-editing/page.tsx
@@ -1,16 +1,15 @@
-import AnimatedTextType from '@/components/common components/animated-text/animated-text-type'
-import { TextType } from '@/components/common components/animated-text/text-type'
-import CTA from '@/components/common components/cta/cta'
-import VideoEditing from '@/components/video-editing/page'
-import React from 'react'
+import CTA from "@/components/common components/cta/cta";
+import VideoGallery from "@/components/video-editing/video-gallery";
+
+import React from "react";
 
 const VideoEditingPage = () => {
   return (
-   <>
-   <VideoEditing/>
-   <CTA/>
-   </>
-  )
-}
+    <>
+      <VideoGallery />
+      <CTA />
+    </>
+  );
+};
 
-export default VideoEditingPage
+export default VideoEditingPage;
